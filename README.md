8528c1d7b41c32da4555b9f35e6c6c7c6e39cc0f
20034951 <20034951@galileo.edu>
2024-03-11 01:41:42 -0600

    Commit inicial

---
+++ README.md
@@ -0,0 +1,24 @@
+# trim1-fcs-act4-php
+# Proyecto: hola mundo desplegando imagen generada por IA
+
+## Descripción 
+Este proyecto es una aplicación elaborada utilizando html5 y PHP.
+
+## Prerrequisitos
+Para poder ejecutar este proyecto en tu entorno local, necesitarás tener instaladas las siguientes tecnologías:
+- apache2 
+- php
+
+## Pasos para su ejecución
+1. Abre una terminal y navega hasta el directorio /var/www/html.
+2. Clona este repositorio en tu máquina local en el directorio arriba indicado.
+3. Listo! Ahora puedes acceder a la aplicación en tu navegador en la dirección [http://localhost/trim1-fcs-act4-php/index.php](http://localhost/trim1-fcs-act4-php/index.php).
+
+
+## Datos del autor
+- Nombre: Pablo Alfonso Vargas Melgar
+- Correo electrónico: 20034951@galileo.edu
+- Carnet: 20034951
+
+
+
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fundamentos de Construccion de Software. -Actividad No.3- Pablo Alfonso Vargas - Carnet: 20034951</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <main role="main">
+            <section class="jumbotron text-center">
+
+                <div class="container">
+                <?php
+
+                    echo "<h1 class=\"jumbotron-heading\">Hola mundo!</h1>";
+                    echo "<h3 class=\"lead text-muted\">soy Pablo Alfonso Vargas, Carnet No. 20034951</h3>";
+
+                ?>
+
+                    <div class="card mx-auto">
+                        <?php
+                            echo "<img class=\"card-img-top\" alt=\"ImagenGeneradaIA\" src=\"images/20034951.png\" style=\"height:360px; width:360px;\">";
+                            echo "<div class=\"card-body\">";
+                            
+                            echo "<p class=\"card-text\">Imagen generada usando IA</p>";
+                            echo "</div>";
+                        ?>
+                    </div>
+
+                </div>
+
+            </section>
+        </main>
+        
+    </body>
+
+</html>
